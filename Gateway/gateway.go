a99dc88af98cecc039e9e2c8b0609e7878f5be30
Ryo Sakuma <SRkuma@users.noreply.github.com>
2020-02-03 15:55:33 +0900

    feat: RowDataGateWayな感じに書きかえ

--- Gateway/gateway.go
+++ Gateway/gateway.go
@@ -2,34 +2,64 @@ package Gateway
 
 import (
 	"database/sql"
-	"fmt"
+	"time"
 )
+
 type Gateway struct {
 	DB sql.DB
 }
 
-type ResultSet struct {
-
-}
-// TODO: asOf to date
-func (g *Gateway) FindRecognitionsFor(contractID int64, asOf string) ResultSet {
-	// findrecognitionstatemet渡してpreparedStatement作る
-	fmt.Println(contractID)
-	fmt.Println(asOf)
-	return ResultSet{}
+type Contract struct {
+	ID         int64
+	Product    int64
+	Revenue    float64
+	DateSigned time.Time
 }
 
-func (g *Gateway) FindContract(contractID int64) ResultSet {
-	findContractStatement :=
-```
+//// TODO: asOf to date
+//func (g *Gateway) FindRecognitionsFor(contractID int64, asOf string) ResultSet {
+//	// findrecognitionstatemet渡してpreparedStatement作る
+//	fmt.Println(contractID)
+//	fmt.Println(asOf)
+//	return ResultSet{}
+//}
+
+func (g *Gateway) FindContract(contractID int64) (*[]Contract, error) {
+
+	findContractStatement := `
 SELECT * FROM
 contracts c,
 product p
 WHERE ID = ?
 AND c.product = p.ID;
-```
-	// findrecognitionstatemet渡してpreparedStatement作る
-	fmt.Println(contractID)
-	return ResultSet{}
-}
+`
+	rows, err := g.DB.Query(findContractStatement, contractID)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
 
+	var (
+		id, product int64
+		revenue     float64
+		dateSigned  time.Time
+	)
+
+	var contracts []Contract
+
+	for rows.Next() {
+		if err := rows.Scan(&id, &product, &revenue, &dateSigned); err != nil {
+			return nil, err
+		}
+		tmp := Contract{
+			ID:         id,
+			Product:    product,
+			Revenue:    revenue,
+			DateSigned: dateSigned,
+		}
+
+		contracts = append(contracts, tmp)
+	}
+
+	return &contracts, nil
+}
